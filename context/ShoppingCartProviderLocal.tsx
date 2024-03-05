e74b1c2bef8165ff9deb9dedd0c5a71d4c39caeb
pranayaryal <drpranayaryal@gmail.com>
2024-03-04 19:11:01 -0500

    localstorage

---
+++ context/ShoppingCartProviderLocal.tsx
@@ -0,0 +1,227 @@
+import { useState, ReactNode, createContext, useContext } from "react";
+import ShoppingCart from "@/components/ShoppingCart";
+import { useLocalStorage } from "@/hooks/useLocalStorage";
+
+
+type ShoppingCartContextLocal = {
+  openCart: () => void
+  closeCart: () => void
+  getItemQuantity: (id: number) => number
+  increaseCartQuantity: (id: number) => void
+  decreaseCartQuantity: (id: number) => void
+  removeCart: (id: number) => void
+  cartQuantity: number
+  cartItems: CartItem[]
+
+}
+
+type CartItem = {
+  id: number
+  quantity: number
+
+}
+
+const ShoppingCartContext = createContext({} as ShoppingCartContextLocal)
+
+export function useShoppingCart() {
+  return useContext(ShoppingCartContext)
+}
+
+export function ShoppingCartProviderLocal({ children }: { children: ReactNode}) {
+  const [cartItems, setCartItems] = useLocalStorage<CartItem[]>(
+    "shopping-cart",
+    []
+  )
+  const [ isOpen, setIsOpen ] = useState(false)
+  const [ product, setProduct ] = useState({})
+
+
+  const openCart = () => setIsOpen(true) 
+  const closeCart = () => setIsOpen(false) 
+
+
+  const increaseCartQuantity = async (id: number) => {
+    const data = { id, quantity: 1 }
+    const response = await fetch('/api/add-cart-item', {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+    const resJson = await response.json()
+    let cartAsArray = []
+    if (resJson.products) {
+      Object.values(resJson.products).map(s => {
+        cartAsArray.push(s)
+      })
+    }
+    setCartItems(cartAsArray);
+
+  }
+
+  const decreaseCartQuantity = async (id: number) => {
+    const data = { id, quantity: 1 }
+    const response = await fetch('/api/decrease-cart-item', {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+    const resJson = await response.json()
+    let cartAsArray = []
+    if (resJson.products) {
+      Object.values(resJson.products).map(s => {
+        cartAsArray.push(s)
+      })
+    }
+    setCartItems(cartAsArray);
+
+  }
+
+  const removeCartItem = async (id: number) => {
+    const data = { id, quantity: 1 }
+    const response = await fetch('/api/remove-cart-item', {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+    const resJson = await response.json()
+    let cartAsArray = []
+    if (resJson.products) {
+      Object.values(resJson.products).map(s => {
+        cartAsArray.push(s)
+      })
+    }
+    setCartItems(cartAsArray);
+
+  }
+
+  const getAllCartItems = async () => {
+    const response = await fetch('/api/get-all-cart', {
+      method: 'GET',
+      headers: {
+        "Content-Type": "application/json",
+      },
+
+    })
+
+    const resJson = await response.json()
+    // console.log('summing', Object.values(resJson.data.products).reduce((a, b) => a + b, 0))
+    // console.log('typeof', resJson.data.products)
+    // Having to set as array
+    let cartAsArray = []
+    if (resJson.products) {
+      Object.values(resJson.products).map(s => {
+        cartAsArray.push(s)
+      })
+    }
+    setCartItems(cartAsArray);
+  }
+
+  const forgetCart = async () => {
+    const response = await fetch('/api/forget-cart', {
+      method: 'GET',
+      headers: {
+        "Content-Type": "application/json",
+      },
+
+    })
+    const resJson = await response.json()
+    setCartItems([]);
+
+  }
+
+  const getProducts = async () => {
+    const response = await fetch('/api/products', {
+      method: 'GET',
+      headers: {
+        "Content-Type": "application/json",
+      }
+
+    })
+
+    const resJson = await response.json();
+
+    setProducts(resJson.products)
+
+  }
+
+  const getProduct = async (id: number) => {
+    const response = await fetch(`/api/product?id=${id}`, {
+      method: 'GET',
+      headers: {
+        "Content-Type": "application/json",
+      }
+
+    })
+
+    const resJson = await response.json();
+    return resJson.products
+
+    // setProducts(resJson.products)
+
+  }
+
+
+
+
+
+  // const decreaseCartQuantity = (id: number) => {
+  //   setCartItems(currItems => {
+  //     // If you don't find add it
+  //     if (currItems.find(item => item.id === id)?.quantity == 1) {
+  //       return currItems.filter(item => item.id !== id)
+  //     }
+
+  //     // If you find it increment by one
+  //     return currItems.map(item => {
+  //       if (item.id == id) {
+  //         return { ...item, quantity: item.quantity - 1 }
+
+  //       }
+  //       return item;
+  //     })
+
+  //   })
+  // }
+
+  // const removeFromCart = (id: number) => {
+  //   setCartItems(currItems => {
+  //     return currItems.filter(item => item.id !== id)
+  //   })
+  // }
+
+  //const cartQuantity = cartItems.reduce((quantity, item) => item.quantity + quantity, 0)
+
+  return (
+    <ShoppingCartContext.Provider
+      value={{
+        // getItemQuantity,
+        openCart,
+        closeCart,
+        products,
+        getProducts,
+        getProduct,
+        setCartItems,
+        increaseCartQuantity,
+        decreaseCartQuantity,
+        getAllCartItems,
+        forgetCart,
+        removeCartItem,
+        // decreaseCartQuantity,
+        // removeFromCart,
+        // openCart,
+        // closeCart,
+        // cartQuantity,
+        cartItems
+      }}>
+      <ShoppingCart isOpen={isOpen} />
+      {/* <SideSlider openSide={isOpen}/> */}
+      {children}
+    </ShoppingCartContext.Provider>
+  )
+}
